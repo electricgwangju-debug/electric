f9ac4bdaf49f40433db5053610a5ff69cb1815ef
electricgwangju-debug <electricgwangju@gmail.com>
2026-09-08 03:21:49 +0900

    feat: Add PrivateRoute component for protected routes

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import useAuthStore from '../stores/authStore'
+
+export default function PrivateRoute({ children }) {
+  const { token } = useAuthStore()
+
+  if (!token) {
+    return <Navigate to="/login" replace />
+  }
+
+  return children
+}
